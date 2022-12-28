4679dce5c53ff9a0c2f942ab8cc0b811635ba818
felix <felix.okoronkwo.1@gmail.com>
2022-12-28 18:41:29 +0100

    The Zen Of Python

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 import sys
-
-def write_to_stderr(*input):
-    sys.stderr.write(*input)
-
-write_to_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
