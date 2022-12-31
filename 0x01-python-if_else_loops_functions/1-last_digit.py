5578243e0c8ce0daf19ccdb8b58b14b58823996e
felix <felix.okoronkwo.1@gmail.com>
2022-12-31 17:59:57 +0100

    last digit printer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,18 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 
-#algorithm to calculate last digit of number
+# algorithm to calculate last digit of number
 if (number > 0):
     lastdigit = number % 10
 elif (number % 10 == 0):
     lastdigit = 0
 else:
-    lastdigit = 10 - (number % 10)
+    lastdigit = -(10 - (number % 10))
 
-#algorithm to print number last digit and state
+# algorithm to print number last digit and state
 if lastdigit > 5:
-    print (f'Last digit of {number} is {lastdigit} and is greater than 5')
+    print(f'Last digit of {number} is {lastdigit} and is greater than 5')
 elif (lastdigit < 6) & (lastdigit != 0):
-    print (f'Last digit of {number} is {lastdigit} and is less than 6 and not 0')
+    print(f'Last digit of {number} is {lastdigit} and is less than 6 and not 0')
 elif lastdigit == 0:
-    print (f'Last digit of {number} is {lastdigit} and is 0')
+    print(f'Last digit of {number} is {lastdigit} and is 0')
