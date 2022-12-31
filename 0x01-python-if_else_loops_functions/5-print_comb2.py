930cc27012b35da79225f42d953f494d5cc17ae8
felix <felix.okoronkwo.1@gmail.com>
2022-12-31 13:59:54 +0100

    print nums

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for num in range(0, 100):
-    if num < 10:
-        print('0', end='')
     if num == 99:
         print('99')
         break
-    print('{}'.format(num), end=', ')
+    print('{:02d}'.format(num), end=', ')
