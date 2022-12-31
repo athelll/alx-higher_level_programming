2d3ac3f8ec4acc71628ea42d3f9b27f75ac53a41
felix <felix.okoronkwo.1@gmail.com>
2022-12-31 18:08:29 +0100

    fixed codestyle of last digit printer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -14,6 +14,7 @@ else:
 if lastdigit > 5:
     print(f'Last digit of {number} is {lastdigit} and is greater than 5')
 elif (lastdigit < 6) & (lastdigit != 0):
-    print(f'Last digit of {number} is {lastdigit} and is less than 6 and not 0')
+    print(f'Last digit of {number} is {lastdigit}', end=" ")
+    print('and is less than 6 and not 0')
 elif lastdigit == 0:
     print(f'Last digit of {number} is {lastdigit} and is 0')
