23aaa2194a05b74c5d001e91131b71ad2ff6882b
felix <felix.okoronkwo.1@gmail.com>
2023-01-01 20:39:04 -0800

    fixed backAlphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@ tracker = 0
 for s in range(ord('z'), ord('a')-1, -1):
     if (tracker % 2 != 0):
         s = s - ord('a') + ord('A')
-    print(chr(s), end='')
+    print('{}'.format(chr(s)), end='')
     tracker += 1
