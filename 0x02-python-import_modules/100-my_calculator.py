36bc531c916ffc98f6ff89cee89868d63fa9566a
felix <felix.okoronkwo.1@gmail.com>
2023-01-03 23:07:56 +0100

    file formatted to fit pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,8 @@ import calculator_1 as calc
 if __name__ == "__main__":
 
     argv = sys.argv[1:]
-    operators = [["+", calc.add], ["-", calc.sub], ["*", calc.mul], ["/", calc.div]]
+    operators = [["+", calc.add], ["-", calc.sub],
+                 ["*", calc.mul], ["/", calc.div]]
 
     if len(argv) != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
@@ -15,7 +16,8 @@ if __name__ == "__main__":
         for j, element in enumerate(sublist):
             if argv[1] == element:
                 result = operators[i][j+1](int(argv[0]), int(argv[2]))
-                print('{} {} {} = {}'.format(argv[0], argv[1], argv[2], result))
+                print('{} {} {} = {}'.format(
+                    argv[0], argv[1], argv[2], result))
                 exit()
 
     print('Unknown operator. Available operators: +, -, * and /')
