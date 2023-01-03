447a5db0288b22d31d0ee85e75497ac7f7820422
felix <felix.okoronkwo.1@gmail.com>
2023-01-03 23:02:47 +0100

    100-my_calculator.py added

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import calculator_1 as calc
+
+if __name__ == "__main__":
+
+    argv = sys.argv[1:]
+    operators = [["+", calc.add], ["-", calc.sub], ["*", calc.mul], ["/", calc.div]]
+
+    if len(argv) != 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+
+    for i, sublist in enumerate(operators):
+        for j, element in enumerate(sublist):
+            if argv[1] == element:
+                result = operators[i][j+1](int(argv[0]), int(argv[2]))
+                print('{} {} {} = {}'.format(argv[0], argv[1], argv[2], result))
+                exit()
+
+    print('Unknown operator. Available operators: +, -, * and /')
+    exit(1)
