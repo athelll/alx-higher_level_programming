4fc7d868eaaaf3b5ad3628de2b0b890538a084c6
felix <felix.okoronkwo.1@gmail.com>
2023-01-03 02:38:56 +0100

    fixed args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argc = len(argv) - 1
-index = 1
+    argc = len(argv) - 1
+    index = 1
 
-if argc == 0:
-    print('{} arguments.'.format(argc))
-elif argc == 1:
-    print('{} argument:'.format(argc))
-else:
-    print('{} arguments:'.format(argc))
+    if argc == 0:
+        print('{} arguments.'.format(argc))
+    elif argc == 1:
+        print('{} argument:'.format(argc))
+    else:
+        print('{} arguments:'.format(argc))
 
-for args in argv[1:]:
-    print('{}: {}'.format(index, args))
-    index += 1
+    for args in argv[1:]:
+        print('{}: {}'.format(index, args))
+        index += 1
