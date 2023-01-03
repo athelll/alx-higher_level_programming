31ccf05041f8705ce691654fcbae4525d31a08f0
felix <felix.okoronkwo.1@gmail.com>
2023-01-03 19:04:09 +0100

    fixed all pycodestyle erros

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     argv = argv[1:]
-    
     if len(argv) == 0:
         print('0 arguments.')
     elif len(argv) == 1:
         print('1 argument:')
     else:
         print('{} arguments:'.format(len(argv)))
-    
     for i, args in enumerate(argv):
         print('{:d}: {:s}'.format(i+1, args))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
-    sum = 0    
+    sum = 0
     for i in range(0, len(argv)):
         sum += int(argv[i])
     print("{:d}".format(sum))
