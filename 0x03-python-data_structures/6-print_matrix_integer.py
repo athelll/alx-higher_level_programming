85540410e450e4d01f635478040931636cfb0924
felix <felix.okoronkwo.1@gmail.com>
2023-01-06 08:48:14 +0100

    casting integers as integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for digit in row:
             if digit == row[-1]:
-                print('{}'.format(digit), end='')
+                print('{:d}'.format(digit), end='')
             else:
-                print('{}'.format(digit), end=' ')
+                print('{:d}'.format(digit), end=' ')
         print()
