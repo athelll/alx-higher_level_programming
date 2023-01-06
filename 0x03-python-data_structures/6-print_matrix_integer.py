95370ba528b33961ecb175829dafb1b5e7af3b54
felix <felix.okoronkwo.1@gmail.com>
2023-01-06 08:44:36 +0100

    matrix printer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for digit in row:
             if digit == row[-1]:
-                print('{}'.format(digit))
+                print('{}'.format(digit), end='')
             else:
                 print('{}'.format(digit), end=' ')
+        print()
