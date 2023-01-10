a41210d1616d7704abb00db672806d6262efa0cd
felix <felix.okoronkwo.1@gmail.com>
2023-01-10 16:20:46 +0100

    reformed to pycodestyle

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for keys, values in sorted(a_dictionary.items()):
-        print('{}: {}'.format(keys,values))
+        print('{}: {}'.format(keys, values))
