223a4fa9ddd7ad2cdffc6f4f7aa667f53cf02e91
felix <felix.okoronkwo.1@gmail.com>
2023-01-10 08:41:35 +0100

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+# def square_matrix_simple(matrix=[]):
+#    new_matrix = []
+#    elements = []
+#    for x in matrix:
+#         elements = []
+#         for y in x:
+#             elements.append(y**2)
+#         new_matrix.append(elements)
+#    return new_matrix
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i, z in enumerate(matrix):
+        new_matrix.append(list(map(lambda y: y**2, matrix[i])))
+    return new_matrix
