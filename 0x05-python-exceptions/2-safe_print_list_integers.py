034e9c67252c45b825593ab70d2da64fbe66730b
felix <felix.okoronkwo.1@gmail.com>
2023-01-13 14:31:41 +0100

    fixed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
             idx += 1
             missed += 1
             continue
-        #except IndexError:
-        #    break
+#       except IndexError:
+#           break
     print()
     return (idx - missed)
