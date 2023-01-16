023d83d3f446357030407cf7d1a733412ac5e640
felix <felix.okoronkwo.1@gmail.com>
2023-01-16 10:49:58 +0100

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/pyhton3
+"""
+Module 3-square
+defines a square with size
+"""
+
+
+class Square:
+    """
+    Square blueprint class
+    
+    Args:
+        size: size of square
+    """
+    def __init__(self, size=0):
+        """
+        initializes a square
+        with size __size
+
+        Args:
+            size: size of square
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        returns the area of the square
+        """
+        return (self.__size)**2
