148438bf0cd0c9eefbb909ea57bf3846f9b1b64c
felix <felix.okoronkwo.1@gmail.com>
2023-01-20 01:00:41 +0100

    fu all

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 4-print_square
+prints square
+"""
+
+
+def print_square(size):
+    """
+    print_square function
+        Args: size <int>
+    """
+    if type(size) is not int or (isinstance(size, float) and  size < 0):
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+
+    print('\n'.join('#'*size for i in range(size)))
