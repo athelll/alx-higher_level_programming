5c249a2546240ff4a5ceb3b228f164d63151fa68
felix <felix.okoronkwo.1@gmail.com>
2023-01-28 10:33:14 +0100

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module: 0-lookup
+Returns the list of available
+Attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """uses the dir() function"""
+    return dir(obj)
