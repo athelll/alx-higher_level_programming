22c65b1970d7060e27667818705e82e5d22d876e
felix <felix.okoronkwo.1@gmail.com>
2023-01-28 16:46:00 +0100

    square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module: 11-square
+
+Inherits from 9-rectangle.py
+The Class Rectangle
+
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """inherits from the rectangle class
+    Methods:
+        __init__(self,size)
+    """
+    def __init__(self, size):
+        """initializes the Square class
+        Args:
+            size (int): private
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        super().area()
+        super().__str__()
