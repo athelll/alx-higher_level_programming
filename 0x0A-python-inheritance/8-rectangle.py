b61f6b35d12da56df4c8cc1b2d9513af38d2c060
felix <felix.okoronkwo.1@gmail.com>
2023-01-28 16:02:50 +0100

    fixed the rectangle class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,7 @@ Contains subclass Rectangle
 """
 
 
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -22,7 +22,7 @@ class Rectangle(BaseGeometry):
             width(int)
             height(int)
         """
-        super().integer_validator("width", self.width)
+        super().integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", self.height)
+        super().integer_validator("height", height)
         self.__height = height
