51694508a5f7c98381a8a7a9cfce7a328dbf463e
felix <felix.okoronkwo.1@gmail.com>
2023-01-28 14:40:32 +0100

    is class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module: 2-is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function returns a boolean if obj is 
+    an instance of a_class
+    """
+
+    return type(obj) == a_class
