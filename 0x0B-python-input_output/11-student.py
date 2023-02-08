36cc867b63ec9d7425a2668c0d7cc226c8a85fcd
6felix <felix.okoronkwo.1@gmail.com>
2023-02-08 22:42:14 +0100

    reload attributes from json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+
+Class: Student
+Function:
+
+    def __init__(self, first_name, last_name, age)
+
+    def to_json(self):
+    returns: dictionary rep. of class
+
+"""
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization Function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json but better"""
+        if attrs is not None:
+            dic = {}
+            for keys in attrs:
+                if keys in self.__dict__.keys():
+                    dic[keys] = self.__dict__[keys]
+            return dic
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """json reload"""
+        for k, v in json.items():
+            setattr(self, k, v)
