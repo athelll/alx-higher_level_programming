3b96458d0a0e14912f1a1edecda62f5dd69f4326
6felix <felix.okoronkwo.1@gmail.com>
2023-02-08 13:15:06 +0100

    fixed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-import json
+"""
+saves json repr.
+of an object to a file
+
+Func: save_to_json_file(my_obj, filename)
+
+"""
+
+
 def save_to_json_file(my_obj, filename):
+    """sfunc saves json to file"""
+    import json
     parsed_json = json.dumps(my_obj)
-    
+
     with open(filename, mode="w") as f:
         f.write(parsed_json)
