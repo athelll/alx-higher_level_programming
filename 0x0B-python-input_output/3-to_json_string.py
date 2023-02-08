a3039e634748815c9a9cf7a57adda3f1c8788dd9
6felix <felix.okoronkwo.1@gmail.com>
2023-02-08 12:50:36 +0100

    to_json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""to_json_string"""
+
+
+def to_json_string(my_str):
+    """to_json_string"""
+    import json
+    json.dumps(my_str)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""to_json_string"""
+
+def to_json_string(my_obj):
+    """json to string"""
+    import json
+    return json.dumps(my_obj)
