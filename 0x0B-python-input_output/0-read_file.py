7c348b573b15a7d37941bff9bafdbb5d9eaa27c3
6felix <felix.okoronkwo.1@gmail.com>
2023-02-08 11:32:55 +0100

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Read_File
 def read_file(filename=""):
     """function that reads file"""
     with open(filename, mode="r", encoding="UTF8") as file:
-        print(file.read())
+        print(file.read(), end="")
