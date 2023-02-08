7ab2a6ddce8b13fd728c9df67537bc083f4a004c
6felix <felix.okoronkwo.1@gmail.com>
2023-02-08 11:12:14 +0100

    read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Read_File"""
+
+
+def read_file(filename=""):
+    """function that reads file"""
+    with open(filename, mode="r", encoding="utf-8") as file:
+        """prints the file"""
+        print(file.read())
