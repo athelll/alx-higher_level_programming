fcf05c553b62337b8de8a115e38a86f8035bcffa
6felix <felix.okoronkwo.1@gmail.com>
2023-03-05 00:02:22 +0000

    how tf

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- number of shows
 
-SELECT tv_genres.name AS genre, COUNT(*) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
