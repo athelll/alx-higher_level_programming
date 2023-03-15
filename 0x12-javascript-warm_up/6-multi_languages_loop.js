a903933f5cb7e6e34401937668a8d70824592ced
6felix <felix.okoronkwo.1@gmail.com>
2023-03-15 14:12:47 +0000

    fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let x in arr) {
-	console.log(arr[x]);
+for (const x in arr) {
+  console.log(arr[x]);
 }
