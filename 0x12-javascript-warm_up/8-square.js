21d5466becf5ea43ec67a7a7df2d49577c090630
6felix <felix.okoronkwo.1@gmail.com>
2023-03-15 14:50:51 +0000

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)){
+	console.log('Missing size');
+} else {
+	let str = 'X'.repeat(x);
+	for (let i = 0; i < x; i++){
+		console.log(str);
+	}
+}
