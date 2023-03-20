5dccb405d2cf5b3cade885f18ae0a3fc8c790085
6felix <felix.okoronkwo.1@gmail.com>
2023-03-20 10:15:41 +0000

    returns all rows

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+retruns all
+rows in table
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states;")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
