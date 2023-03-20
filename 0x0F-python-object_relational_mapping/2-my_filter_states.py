2683b1c5b1adaf7dea199a8ee5d504c43f757c09
6felix <felix.okoronkwo.1@gmail.com>
2023-03-20 01:10:29 +0000

    arizona

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ something
 import MySQLdb
 from sys import argv
 
-if __name__ == "__ main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
@@ -15,13 +15,15 @@ if __name__ == "__ main__":
                          db=argv[3])
 
     cursor = db.cursor()
-    prompt = "SELECT * FROM states WHERE name = {} ORDER BY id ASC".format(argv[4])
+    prompt = """SELECT *
+             FROM states
+             WHERE name = '{}'
+             ORDER BY id ASC""".format(argv[4])
 
     cursor.execute(prompt)
 
     for row in cursor.fetchall():
-         if row[1] == argv[4]:
-            print(row)
+        print(row)
 
     cursor.close()
     db.close()
