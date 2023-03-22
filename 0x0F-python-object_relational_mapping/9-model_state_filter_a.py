2b279408b0741309f9310f7f366fada595f2cfee
6felix <felix.okoronkwo.1@gmail.com>
2023-03-22 18:10:24 +0000

    names that have 'a' in them

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+queries the local database
+for all state row entries
+that conatin the letter "a"
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).order_by(State.id).filter(
+            State.name.ilike('%a%'))
+
+    for user in result:
+        print("{}: {}".format(user.id, user.name))
+
+    session.close()
