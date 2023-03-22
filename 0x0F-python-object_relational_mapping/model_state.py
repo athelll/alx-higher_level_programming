a3b892829df412dcd34a5d3e957f8962db8c276e
6felix <felix.okoronkwo.1@gmail.com>
2023-03-22 10:32:55 +0000

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,8 +5,9 @@ Defines Class states
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
+
+
 class State(Base):
     """
      Creatse the schema for
