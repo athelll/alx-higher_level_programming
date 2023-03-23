e3b3036dd9fe83376d53ad0759f0c2554b157d23
6felix <felix.okoronkwo.1@gmail.com>
2023-03-23 23:02:00 +0000

    relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+creates city and
+states objects with
+a relationship that affects
+each other and commits it to
+the DB
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new = State(name='California')
+    new2 = City(name="San Francisco")
+    new.cities.append(new2)
+
+    session.add(new)
+    session.add(new2)
+
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines the class
+City which is the schema
+of the table cities
+"""
+
+from relationship_state import Base, State
+from sqlalchemy import Integer, String, Column, ForeignKey
+
+
+class City(Base):
+    """City Schema class"""
+
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True)  # autoincrements
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
