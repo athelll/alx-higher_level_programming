6482cd64ac716d17b3ee830a2c39db8286394e3c
6felix <felix.okoronkwo.1@gmail.com>
2023-03-23 23:08:10 +0000

    fuck you all

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
 
-    Base.metadata.create_all(engine)    
+    Base.metadata.create_all(engine) 
     Session = sessionmaker(bind=engine)
     session = Session()
 
