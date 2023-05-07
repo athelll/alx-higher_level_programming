f6c12d98340ef620f365685d4a9cbace1d7a473b
6felix <felix.okoronkwo.1@gmail.com>
2023-05-07 12:18:41 +0000

    sends a DELETE request to a server

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to a URL passed as the
-# first argument and displays the body of the response
+# sends a DELETE request to a URL passed as first argument and displays the body of the response
 curl -sX DELETE "$1"
