fb9414880650889dac21d46dda8e3b2a8409f4c5
6felix <felix.okoronkwo.1@gmail.com>
2023-05-09 23:00:25 +0100

    request from a server and prints error code or response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL
+and displays the body of the response. if an
+error arises it prints the error code
+Package: `requests`
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
+        print('Error code: '.format(response.status_code))
+    else:
+        print(response.text)
