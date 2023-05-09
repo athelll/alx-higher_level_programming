275c4a6e080a0f40023b9ec174ec8fe09a60f095
6felix <felix.okoronkwo.1@gmail.com>
2023-05-09 23:03:03 +0100

    request from a server and prints error code or response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,6 +12,6 @@ import requests
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
     if response.status_code >= 400:
-        print('Error code: '.format(response.status_code))
+        print('Error code: {}'.format(response.status_code))
     else:
         print(response.text)
